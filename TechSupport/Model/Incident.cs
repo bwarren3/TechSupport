fb24d0ba636fc22e5f6463b8e4e10786568896b2
bwarren3 <bwarren3@my.westga.edu>
2026-02-02 19:07:21 -0500

    Added the Incident Model

---
+++ TechSupport/Model/Incident.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport.Model
+{
+    /// <summary>
+    /// Represents a tech support incident.
+    /// </summary>
+    public class Incident
+    {
+        /// <summary>
+        /// Gets or sets the incident ID.
+        /// </summary>
+        public int IncidentId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the incident title.
+        /// </summary>
+        public string Title { get; set; } = "";
+
+        /// <summary>
+        /// Gets or sets the incident description.
+        /// </summary>
+        public string Description { get; set; } = "";
+
+        /// <summary>
+        /// Gets or sets the customer ID for this incident.
+        /// </summary>
+        public int CustomerId { get; set; }
+    }
+}
+
