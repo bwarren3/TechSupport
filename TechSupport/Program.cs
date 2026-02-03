b0c49752d98d032b8e04517ab300bcce65b70bcd
bwarren3 <bwarren3@my.westga.edu>
2026-02-02 20:33:33 -0500

    Updated the Program.cs to match the requirements of the assignment

--- TechSupport/Program.cs
+++ TechSupport/Program.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Windows.Forms;
+using TechSupport.View;
+
 namespace TechSupport
 {
     internal static class Program
@@ -11,7 +15,7 @@ namespace TechSupport
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
