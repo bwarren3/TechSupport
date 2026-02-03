4b42b656c86b861adb7052e78f57de00d9186bf8
bwarren3 <bwarren3@my.westga.edu>
2026-02-02 20:32:22 -0500

    Created the IncidentRepository

---
+++ TechSupport/DAL/IncidentRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechSupport.Model;
+
+namespace TechSupport.DAL
+{
+    /// <summary>
+    /// Simulates persistent data storage for incidents.
+    /// </summary>
+    public static class IncidentRepository
+    {
+        private static readonly List<Incident> incidents = new();
+        private static int nextId = 1;
+
+        /// <summary>
+        /// Gets all incidents.
+        /// </summary>
+        /// <returns>All incidents.</returns>
+        public static List<Incident> GetAll()
+        {
+            return incidents.ToList();
+        }
+
+        /// <summary>
+        /// Adds an incident to the repository.
+        /// </summary>
+        /// <param name="incident">The incident to add.</param>
+        public static void Add(Incident incident)
+        {
+            incident.IncidentId = nextId++;
+            incidents.Add(incident);
+        }
+
+        /// <summary>
+        /// Gets incidents for a specific customer.
+        /// </summary>
+        /// <param name="customerId">Customer ID.</param>
+        /// <returns>Matching incidents.</returns>
+        public static List<Incident> GetByCustomerId(int customerId)
+        {
+            return incidents.Where(i => i.CustomerId == customerId).ToList();
+        }
+    }
+}
