d93416ee6d4af9b468af661a28218844c1c3190f
bwarren3 <bwarren3@my.westga.edu>
2026-02-02 20:31:57 -0500

    Added the IncidentController

---
+++ TechSupport/Controller/IncidentController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechSupport.DAL;
+using TechSupport.Model;
+
+namespace TechSupport.Controller
+{
+    /// <summary>
+    /// Handles business logic related to incidents.
+    /// </summary>
+    public class IncidentController
+    {
+        /// <summary>
+        /// Gets all incidents.
+        /// </summary>
+        /// <returns>A list of all incidents.</returns>
+        public List<Incident> GetAllIncidents()
+        {
+            return IncidentRepository.GetAll();
+        }
+
+        /// <summary>
+        /// Adds a new incident.
+        /// </summary>
+        /// <param name="incident">The incident to add.</param>
+        public void AddIncident(Incident incident)
+        {
+            IncidentRepository.Add(incident);
+        }
+
+        /// <summary>
+        /// Searches incidents by customer ID.
+        /// </summary>
+        /// <param name="customerId">The customer ID.</param>
+        /// <returns>A list of matching incidents.</returns>
+        public List<Incident> SearchIncidentsByCustomerId(int customerId)
+        {
+            return IncidentRepository.GetByCustomerId(customerId);
+        }
+    }
+}
